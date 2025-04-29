d265f7e11cb494473d71f0406eb829e88f7766a7
serhioses <serega.mirosh@gmail.com>
2025-04-29 10:14:47 +0300

    Update chat users response

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -2,14 +2,19 @@ import { z } from 'zod';
 import { cloudinary } from '../lib/cloudinary.js';
 import { Message } from '../models/message.model.js';
 import { User } from '../models/user.model.js';
+import { UserDTO } from '../dtos/user.dto.js';
 
 export async function getChatUsers(req, res) {
-    const userId = req?.user?.userId;
+    const userId = req?.user?.id;
 
     try {
         const users = await User.find({ _id: { $ne: userId } }).select(['-password', '-updatedAt', '-salt']);
 
-        res.status(200).json({ data: { users } });
+        const usersDTO = users.map((u) => {
+            return new UserDTO(u);
+        });
+
+        res.status(200).json({ data: usersDTO });
     } catch (error) {
         res.status(500).json({ errors: [{ message: 'Internal server error.' }] });
     }
@@ -17,7 +22,7 @@ export async function getChatUsers(req, res) {
 
 export async function getChatMessages(req, res) {
     const { contactId } = req.params;
-    const userId = req?.user?.userId;
+    const userId = req?.user?.id;
 
     if (!contactId || !userId) {
         res.status(400).json({ errors: [{ message: 'Bad request.' }] });
@@ -59,7 +64,7 @@ export async function sendChatMessage(req, res) {
     const { text } = req.body;
     const image = req.file;
     const { contactId } = req.params;
-    const userId = req?.user?.userId;
+    const userId = req?.user?.id;
 
     if (!contactId || !userId) {
         return res.status(400).json({ errors: [{ message: 'Bad request.' }] });
