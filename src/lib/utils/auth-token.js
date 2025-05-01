ac72b11cf2d56af5a710b9c2c59551cfc9b64c32
serhioses <serega.mirosh@gmail.com>
2025-05-01 23:03:19 +0300

    Fix cookies

--- src/lib/utils/auth-token.js
+++ src/lib/utils/auth-token.js
@@ -8,7 +8,7 @@ export function generateToken(user, res) {
     res.cookie('auth_token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'none',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
     });
 
