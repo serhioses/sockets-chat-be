2b3d7e249a179094dcad12d345840c07c0a993e0
serhioses <serega.mirosh@gmail.com>
2025-05-01 20:54:59 +0300

    Revert cookie

--- src/lib/utils/auth-token.js
+++ src/lib/utils/auth-token.js
@@ -7,7 +7,7 @@ export function generateToken(user, res) {
 
     res.cookie('auth_token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,
         sameSite: 'lax',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7d
     });
